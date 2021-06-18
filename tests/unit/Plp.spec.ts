c27d43d9380e218024d9aa62c5a5979e702dd32b
Edoardo Gargano <edoardo.gargano@monrif.net>
2021-06-18 14:52:08 +0200

    some simple test to expand

---
+++ tests/unit/Plp.spec.ts
@@ -0,0 +1,13 @@
+import { shallowMount } from "@vue/test-utils";
+import Plp from "../../src/components/Plp/Plp.vue";
+
+describe("Plp.vue", () => {
+  it("renders title of page", () => {
+    const wrapper = shallowMount(Plp);
+    expect(wrapper.find('.plp-title').text()).toMatch('Products');
+  });
+  it("renders a product", () => {
+    const wrapper = shallowMount(Plp);
+    expect(wrapper.find('.product').html()).toBeTruthy();
+  });
+});
